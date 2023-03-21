b5e35d5aa97e8d6c4c662079109f84d7d3b7f32b
MichalPokojny <michal.pokojny@hotmail.com>
2023-03-21 16:01:27 +0000

    hide api key in contact.html

--- info/views.py
+++ info/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.conf import settings
 
 
 class Home(TemplateView):
@@ -8,6 +9,11 @@ class Home(TemplateView):
 
 class Contact(TemplateView):
     template_name = "contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['API_KEY'] = settings.EMAIL_API_KEY
+        return context
 
 
 class About(TemplateView):
