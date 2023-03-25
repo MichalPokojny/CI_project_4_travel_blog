1700ed41317c68b992d9e5f93af1e39cbe8794d2
MichalPokojny <michal.pokojny@hotmail.com>
2023-03-25 11:56:03 +0000

    add comments to email.js

--- static/js/email.js
+++ static/js/email.js
@@ -1,6 +1,8 @@
+// function to send email using emailJS
 function sendMail(contactForm) {
+    // call the emailJS send function with the appropriate parameters
     emailjs.send("gmail", "Michal", {
-
+        // Extract form data and pass it as emailJS template parameters
         "email_enquiry" : contactForm.floatingSelect.value,
         "from_name": contactForm.firstname.value,
         "last_name": contactForm.lastname.value,
@@ -9,11 +11,13 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
+            // If the email is sent successfully, log a success mesage
             console.log("SUCCESS", response);
             alert(`Thank you ${contactForm.firstname.value}, your email has been sent!`);
             location.reload();
         },
         function(error) {
+            // if there's error sending the email, log the err mesage
             console.log("FAILED", error);
             alert("Email hasn't been sent!");
         }
