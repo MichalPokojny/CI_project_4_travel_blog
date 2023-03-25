eb6792afbb603fb2880a91405340c281bc77513f
MichalPokojny <michal.pokojny@hotmail.com>
2023-03-25 11:46:01 +0000

    add comment to context_processor

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,4 +2,9 @@ from .models import Category
 
 
 def categories(request):
+    """
+    Define a method that takes a request object and return a dictrionary
+    containing all the category objects in the database.
+    This will be availible as context processor to all templates
+    """
     return {'cat_items': Category.objects.all()}
