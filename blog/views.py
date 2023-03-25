a45db3f43e0c2f83db03e00155fa95625a5b1934
MichalPokojny <michal.pokojny@hotmail.com>
2023-03-25 11:36:19 +0000

    add comments to the views.py

--- blog/views.py
+++ blog/views.py
@@ -11,54 +11,96 @@ from django.contrib import messages
 
 
 def search_posts(request):
+    """
+    Method for searching for posts based on the title or author name
+    """
     if request.method == 'POST':
+        # Getting the search query from the form data
         searched = request.POST.get('searched', '')
+
+        # Filtering the Post model based on the title or author name
         posts = Post.objects.filter(Q(title__contains=searched) | Q(author__username__contains=searched))
+
         return render(request, 'search_posts.html', {'searched': searched, 'posts': posts})
-    else: 
-        return render(request, 'search_posts.html', {})
+    else:
+        message = "Search unsuccessful"
+        return render(request, 'search_posts.html', {'message': message})
 
 
 class CategoryView(ListView):
+    """
+    Class based view for displaying posts in selected category
+    """
     model = Post
     template_name = 'categories.html'
+    # Name of the context variable to be used in the template
     context_object_name = 'category_posts'
     paginate_by = 5
 
     def get_queryset(self):
+        """
+        Overriding the get_queryset method to filter posts based on the
+        category selected and display them in descending order
+        of creation date + add comment count.
+        """
         cat = self.kwargs['cat']
         return Post.objects.filter(category=cat, status=1).order_by('-created_on').annotate(comment_count=Count('comments'))
 
     def get_context_data(self, **kwargs):
+        """
+        Overriding the get_context_data method to add the category name 
+        to the context data
+        """
         context = super().get_context_data(**kwargs)
         context['cat'] = self.kwargs['cat']
         return context   
 
 
 class CreatePostView(CreateView):
+    """
+    Class based view for creating new post
+    """
     model = Post
     form_class = PostForm
     template_name = 'create_post.html'
 
     def form_valid(self, form):
+        """
+        Overriding the form valid method to set the author of the post
+        to the current logged in user
+        """
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
 class UpdatePostView(UpdateView):
+    """
+    Class based view for updating existing post
+    """
     model = Post
     form_class = PostForm
     template_name = 'edit_post.html'
 
     def form_valid(self, form):
+        """
+        Overriding the form valid method to set the author of the post
+        to the current logged in user
+        """
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     def update_post(request, slug):
+        """
+        Method for update the post and return a response
+        """
+        # Get the post instance from the database
         post = get_object_or_404(Post, slug=slug)
+        # Check if the current user is the author of the post or staff
         if request.user != post.author and not request.user.is_staff:
+            # If not return a forbidden response
             return HttpResponseForbidden()
         if request.method == 'POST':
+            # Create a new form instance with the updated data
             form = PostForm(request.POST, request.FILES, instance=post)
             if form.isvalid():
                 post = form.save(commit=False)
@@ -71,12 +113,18 @@ class UpdatePostView(UpdateView):
 
 
 class DeletePostView(DeleteView):
+    """
+    Class based view for deleting the post
+    """
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('blog')
 
 
 class PostList(ListView):
+    """
+    Class based view for viewing all the posts
+    """
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on').annotate(comment_count=Count('comments'))
     template_name = 'blog.html'
@@ -85,6 +133,9 @@ class PostList(ListView):
 
 
 class PostLike(View):
+    """
+    Add or remove like for a post
+    """
     def post(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
 
@@ -97,15 +148,22 @@ class PostLike(View):
 
 
 class PostDetail(View):
-
+    """
+    Class based view of a blog post with all the details
+    """
     def get(self, request, slug, *args, **kwargs):
+        # Filters the published blog posts and count num of comments
         queryset = Post.objects.filter(status=1).annotate(comment_count=Count('comments'))
+        # Retrieves a post with given slug or raises 404 err if doesnt exist
         post = get_object_or_404(queryset, slug=slug)
+        # Retrieves the approved comments for the post
         comments = post.comments.filter(approved=True)
+        # Check if logged in user has liked the post or not
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
+        # renders the post detail view with comments, liked status,comment form
         return render(
             request,
             "post_detail.html",
@@ -126,8 +184,8 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
+        # Creates a new comment instance and saves it to the database if valid
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user
@@ -135,6 +193,7 @@ class PostDetail(View):
             comment.post = post
             comment.save()
         else:
+            # Form with the submitted data and errors
             comment_form = CommentForm()    
 
         return render(
@@ -152,12 +211,20 @@ class PostDetail(View):
 
 @login_required
 def profile_view(request):
-
+    """
+    Method for retrieve and displaying user object for
+    the logged in user or raises 404 err if not existing.
+    If the user is not logged in @login_required decorator
+    ensures that the use is redirected to the login page.
+    """
     user = get_object_or_404(User, username=request.user.username)
+    # retrieves blog posts of the user
     posts = Post.objects.filter(author=user)
+    # provides the form with the user data to update the profile info
     form = UserUpdateForm(instance=request.user)
 
     if request.method == 'POST':
+        # Processes te form submission to update user info
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
@@ -165,6 +232,7 @@ def profile_view(request):
             return HttpResponseRedirect(reverse(profile_view))
         else:
             form = UserUpdateForm(instance=request.user)
+    # create a context dictionary with the user object, posts and form
     context = {'user': user, 'posts': posts, 'form': form}
-
+    # render the profile page with the context dictionary
     return render(request, 'profile.html', context)
