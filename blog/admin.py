b52a4c96392ff6de14dbc7de67070f2f76731020
MichalPokojny <michal.pokojny@hotmail.com>
2023-03-25 11:41:56 +0000

    add comments to the admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from .models import Post, Comment, Category
 from django_summernote.admin import SummernoteModelAdmin
 
+# registering the category model to the admin site
 admin.site.register(Category)
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """
+    Registering the Post model with the admin site
+    using the SummerNoteModelAdmin
+    """
     list_display = ('title', 'slug', 'status', 'created_on', 'author')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
@@ -17,6 +21,9 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Registering the comment model to the admin site
+    """
 
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
@@ -24,4 +31,5 @@ class CommentAdmin(admin.ModelAdmin):
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
+        # method used to approve selected comments
         queryset.update(approved=True)
